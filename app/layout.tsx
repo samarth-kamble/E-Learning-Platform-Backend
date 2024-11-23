aa53941715141384d383481cd9f80e6162bcb4f8
Samarth Kamble <kamblesamarth04@gmail.com>
2024-11-23 21:27:15 +0530

    Add input and label components; add sign in and sign up page ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            <Navbar />
+            {/* <Navbar /> */}
             {children}
           </ThemeProvider>
         </body>
